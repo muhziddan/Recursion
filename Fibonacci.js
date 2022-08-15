ede7e57c13262f3ef31cc72e54041ba54db9b4a3
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-08-15 16:54:47 +0700

    add question and fibonacci function

---
+++ Fibonacci.js
@@ -0,0 +1,12 @@
+// Given a number N, return the index value of the fibonacci sequence, where the sequence is:
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+// the pattern values, that means that for n=5 -> 2 + 3
+
+function iterativeFibonacci(value) {
+    return 0
+}
+
+function recursiveFibonacci(value) {
+    return 0
+}
