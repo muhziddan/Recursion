7b26ae021a3ec3f21ea129bc27b774d80a81f9a8
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-08-15 19:59:15 +0700

    finished making reverse string using iterative way

--- ReverseString.js
+++ ReverseString.js
@@ -1,7 +1,13 @@
 //Implement a function that reverses a string using iteration...and then recursion!
 
 function reverseString(str) {
-    return ""
+    let result = []
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        result.push(str[i])
+    }
+
+    return result.join('')
 }
 
-reverseString("test")
+console.log(reverseString("test"))
